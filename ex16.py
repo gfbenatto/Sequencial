e95b91f7718e9378aa4148f152e587b833668ead
torlax <torlax@gmail.com>
2015-06-01 19:41:22 -0300

    Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.

---
+++ ex16.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+metros = input("Informe quantos metros serao pintados. ")
+
+litros = metros/3
+
+preco = 80.0
+capacidadel = 18.0
+
+latas = litros/capacidadel
+total = latas*capacidadel
+
+print("Voce usara ", latas , "latas de tinta")
+print("O valor total e de: ", total, "reais")
