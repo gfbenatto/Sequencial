a9f5c21a52aeae796e191835e4f5052e2cb679c6
torlax <torlax@gmail.com>
2015-06-01 19:39:16 -0300

    Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58

---
+++ ex12.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+#Tendo como dados de entrada a altura de uma pessoa, 
+#construa um algoritmo que calcule seu peso ideal
+ 
+
+altura = input("Digite sua altura. ")
+
+peso = (72.7*altura)-58
+
+print("Seu peso ideal e: ", peso)
