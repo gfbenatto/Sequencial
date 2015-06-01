686a0a6ceac7ce58b25f9c07af7ba6eb52bd7fd0
torlax <torlax@gmail.com>
2015-06-01 19:34:19 -0300

    Faça um Programa que peça as 4 notas bimestrais e mostre a média.

---
+++ ex4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+nota1 = input("Digite a primeira nota ")
+nota2 = input("Digite a segunda nota ")
+nota3 = input("Digite a terceira nota ")
+nota4 = input("Digite a quarta nota ")
+
+media = (nota1+nota2+nota3+nota4)/4
+
+print("A media do aluno e ", float(media))
