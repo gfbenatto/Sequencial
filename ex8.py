5ec023775dfb10e50b7e95172032a92b4e83636e
torlax <torlax@gmail.com>
2015-06-01 19:37:34 -0300

    Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.

---
+++ ex8.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+salario = input("Informe quanto voce ganha por hora.")
+horas = input("Informe a quantidade de horas trabalhadas neste mes.")
+
+total = salario*horas
+
+print("O valor do seu salario este mes e: ",float(total))
